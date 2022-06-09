a071312dff61e00e6b84bfa896443882867e34c1
SagarDNG <sagardng123@gmail.com>
2022-06-09 12:46:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+alert('Please click the button, to change the background color of the page');
+alert('The RGB value of the same color can be seen on the top of the page');
+const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+button.addEventListener('click', () => {
+    makeRandColor();
+})
+
+const makeRandColor = (color) => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const newColor = `rgb(${r}, ${g}, ${b})`;
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+    const sumAll = r + g + b;
+    const sumRG = r + g;
+    if (sumAll < 120 || sumRG < 110) {
+        h1.style.color = 'white';
+    } else {
+        h1.style.color = 'black';
+    }
+
+}
+
